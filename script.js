9022b94239f6c9d4c5cf051743000ed9b80500bc
jdtroubat <jd.troubat@gmail.com>
2023-03-18 19:15:20 +0100

    Functionnal Undress

--- script.js
+++ script.js
@@ -2,20 +2,36 @@ const btnClear = document.getElementById("btnClear");
 const btnResize = document.getElementById("btnResize");
 const gridContainer = document.getElementById("gridContainer");
 
-
-
-btnClear = addEventListener("click", clearGrid);
-btnResize = addEventListener("click", resizeGrid);
-
-
 function clearGrid() {
-
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
 }
 
 function resizeGrid() {
-
+    let size = prompt("Enter the new size (1-100):");
+    if (size !== null && size >= 1 && size <= 100) {
+        gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+        clearGrid();
+        createGrid(size);
+    }
 }
 
-function createGrid() {
-
+function createGrid(size = 16) {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const cell = document.createElement("div");
+            cell.classList.add("grid-cell");
+            cell.addEventListener("mouseover", () => {
+                cell.style.backgroundColor = "black";
+            });
+            gridContainer.appendChild(cell);
+        }
+    }
 }
+
+btnClear.addEventListener("click", clearGrid);
+btnResize.addEventListener("click", resizeGrid);
+
+createGrid();
