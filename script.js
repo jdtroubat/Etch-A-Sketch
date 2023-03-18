ef6462e72ae10bf9ba86abe8c846c4d3477bb819
jdtroubat <jd.troubat@gmail.com>
2023-03-18 20:15:21 +0100

    Fix mouseover not coloring cells after resizing

--- script.js
+++ script.js
@@ -2,12 +2,6 @@ const btnClear = document.getElementById("btnClear");
 const btnResize = document.getElementById("btnResize");
 const gridContainer = document.getElementById("gridContainer");
 
-function clearGrid() {
-    while (gridContainer.firstChild) {
-        gridContainer.removeChild(gridContainer.firstChild);
-    }
-}
-
 function resizeGrid() {
     let size = prompt("Enter the new size (1-100):");
     if (size !== null && size >= 1 && size <= 100) {
@@ -18,14 +12,26 @@ function resizeGrid() {
     }
 }
 
+function addMouseoverEventToCell(cell) {
+    cell.addEventListener("mouseover", () => {
+        cell.style.backgroundColor = "black";
+    });
+}
+
+function clearGrid() {
+    const currentSize = getComputedStyle(gridContainer).gridTemplateColumns.split(" ").length;
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+    createGrid(currentSize);
+}
+
 function createGrid(size = 16) {
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
             const cell = document.createElement("div");
             cell.classList.add("grid-cell");
-            cell.addEventListener("mouseover", () => {
-                cell.style.backgroundColor = "black";
-            });
+            addMouseoverEventToCell(cell);
             gridContainer.appendChild(cell);
         }
     }
