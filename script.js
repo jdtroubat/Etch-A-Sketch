bc20446a154454f2e49ac2f614c408764eefc8e6
jdtroubat <jd.troubat@gmail.com>
2023-03-18 17:11:26 +0100

    Structure HTML + Js

---
+++ script.js
@@ -0,0 +1,21 @@
+const btnClear = document.getElementById("btnClear");
+const btnResize = document.getElementById("btnResize");
+const gridContainer = document.getElementById("gridContainer");
+
+
+
+btnClear = addEventListener("click", clearGrid);
+btnResize = addEventListener("click", resizeGrid);
+
+
+function clearGrid() {
+
+}
+
+function resizeGrid() {
+
+}
+
+function createGrid() {
+
+}
